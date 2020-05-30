c3e6e765c093cb3b8f1121cb715e6fbca29800a7
Jonathan Perry <jonathanperry@sequencemedia.net>
2020-05-30 18:00:45 +0100

    Initial commit

---
+++ stories/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import ToZashiki from 'shinkansen-pinion'
+
+const definition = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  properties: {
+    street_address: {
+      type: 'string',
+      title: 'Address (Camden - Question 1)',
+      description: 'Number and street name'
+    },
+    city: {
+      type: 'string'
+    },
+    state: {
+      type: 'string'
+    }
+  },
+  required: [
+    'street_address',
+    'city',
+    'state'
+  ]
+}
+
+storiesOf('CheckboxCog', module)
+  .add('Required and default props', () => (
+    <ToZashiki definition={definition} />
+  ))
+  .add('Additional props', () => (
+    <ToZashiki definition={definition} />
+  ))
