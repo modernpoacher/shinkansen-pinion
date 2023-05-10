9c1d4a2b814eedf14a89882b1cdf573dddb1478e
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-10 21:22:27 +0100

    Exports

--- src/index.cjs
+++ src/index.cjs
@@ -8,9 +8,7 @@ log('`shinkansen` is awake')
 
 const components = require('./components/index.mjs')
 const transformers = require('./transformers/index.mjs')
-const {
-  default: Pinion
-} = require('./pinion/index.cjs')
+const Pinion = require('./pinion/index.cjs')
 
 module.exports.components = components
 module.exports.transformers = transformers
