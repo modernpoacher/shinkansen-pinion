674c2c76d467bdd5e630c8da8aceb0aee9220c3c
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-31 19:12:26 +0100

    Amends error summary transformer `uri`

--- src/transformers/error-message/index.mjs
+++ src/transformers/error-message/index.mjs
@@ -5,11 +5,11 @@ const log = debug('shinkansen-pinion/transformers/error-message')
 export function hasError (errors = [], uri = null) {
   log('hasError')
 
-  return errors.some(({ uri: URI }) => URI === uri)
+  return errors.some(({ uri: URI }) => '#'.concat(URI) === uri)
 }
 
 export function getError (errors = [], uri = null) {
   log('getError')
 
-  return errors.find(({ uri: URI }) => URI === uri)
+  return errors.find(({ uri: URI }) => '#'.concat(URI) === uri)
 }
