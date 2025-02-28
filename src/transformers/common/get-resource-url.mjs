7eadfff8b2900a12c071c96159e37b0784888577
Jonathan Perry <jonathanperry@modernpoacher.com>
2025-02-28 19:46:58 +0000

    `Signals` `Rails`

--- src/transformers/common/get-resource-url.mjs
+++ src/transformers/common/get-resource-url.mjs
@@ -1,12 +1,12 @@
 import debug from 'debug'
 
 import {
-  Rails
-} from 'shinkansen-rails'
-
-import {
   Signals
 } from 'shinkansen-signals'
+
+import {
+  Rails
+} from 'shinkansen-rails'
 
 const log = debug('shinkansen-pinion/transformers/check-answers')
 
