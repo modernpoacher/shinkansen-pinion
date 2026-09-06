f02bf7a4f7d3fd6b224fd2b0197808f86ab59c98
Jonathan Perry <jonathanperry@modernpoacher.com>
2026-09-06 19:55:56 +0100

    Jest

--- src/js/pinion/__tests__/index.test.jsx
+++ src/js/pinion/__tests__/index.test.jsx
@@ -101,6 +101,14 @@ jest.mock('react-router', () => {
 })
 
 describe('#pinion/pinion', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
   const MOCK_PINION = {
     meta: { type: 'MOCK', uri: '#/' },
     elements: { title: 'MOCK TITLE', field: { id: 'MOCK ID' } }
